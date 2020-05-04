a2a2f18c987b55aace72297c36556fa0863167ea
Jack-42 <jakob-robert@hotmail.de>
2020-05-04 02:15:11 +0200

    Add service worker (listening to events)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,10 @@
+// register service worker if it is supported
 if ('serviceWorker' in navigator) {
     // Notice: url is relative to root, not relative to this file (../sw.js did not work)
     navigator.serviceWorker.register('/tone-generator-pwa/sw.js')
-        .then(reg => console.log('service worker registered', reg))
-        .catch(err => console.error('service worker not registered!', err));
+        .then(reg => {
+            console.log('service worker registered', reg);
+        }).catch(err => {
+            console.error('service worker not registered', err);
+    });
 }
