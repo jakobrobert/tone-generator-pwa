4a903849748e948d81f9892b1959389ec7f8954a
Jack-42 <jakob-robert@hotmail.de>
2020-05-05 00:58:29 +0200

    Generate example sine tone

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -8,3 +8,25 @@ if ('serviceWorker' in navigator) {
             console.error('service worker not registered', err);
     });
 }
+
+function start() {
+    const ctx = new window.AudioContext();
+
+    const frequency = 440.0;
+    const amplitude = 1.0;
+    const duration = 1.0;
+    const numSamples = ctx.sampleRate * duration;
+    const buffer = ctx.createBuffer(1, numSamples, ctx.sampleRate);
+
+    // sine tone
+    const data = buffer.getChannelData(0);
+    for (let i = 0; i < buffer.length; i++) {
+        const time = i / ctx.sampleRate;
+        data[i] = amplitude * Math.sin(2.0 * Math.PI * frequency * time);
+    }
+
+    const source = ctx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(ctx.destination);
+    source.start();
+}
