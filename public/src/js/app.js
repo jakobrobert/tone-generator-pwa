b3fbbd7ac25592655e473aa8ab792a0a76a874a3
Jack-42 <jakob-robert@hotmail.de>
2020-05-05 02:49:34 +0200

    #2 Add Simple Sine Generator (#5)
    * Generate example sine tone
    
    * Loop sound, add stop button
    
    * Add frequency and amplitude slider including labels showing value (but without functionality)
    
    * Update labels for initial slider values
    
    * Stop automatically before re-starting to fix bug of multiple playbacks at once
    
    * Apply values of sliders for tone generation

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,10 @@
+let ctx;
+let buffer;
+let source;
+
+let frequency;
+let amplitude;
+
 // register service worker if it is supported
 if ('serviceWorker' in navigator) {
     // Notice: url is relative to root, not relative to this file (../sw.js did not work)
@@ -7,4 +14,59 @@ if ('serviceWorker' in navigator) {
         }).catch(err => {
             console.error('service worker not registered', err);
     });
+}
+
+// update for initial slider values
+onFrequencyChanged();
+onAmplitudeChanged();
+
+function start() {
+    // stop before re-starting to avoid multiple playbacks at once
+    stop();
+
+    // simply re-generate for each start
+    // could optimize later
+    generateSine();
+
+    source = ctx.createBufferSource();
+    source.buffer = buffer;
+    source.loop = true;
+    source.connect(ctx.destination);
+    source.start();
+}
+
+function stop() {
+    if (!source) {
+        return;
+    }
+    source.stop();
+}
+
+function generateSine() {
+    ctx = new window.AudioContext();
+
+    const duration = 1.0;
+    const numSamples = ctx.sampleRate * duration;
+    buffer = ctx.createBuffer(1, numSamples, ctx.sampleRate);
+
+    // sine tone
+    const data = buffer.getChannelData(0);
+    for (let i = 0; i < buffer.length; i++) {
+        const time = i / ctx.sampleRate;
+        data[i] = amplitude * Math.sin(2.0 * Math.PI * frequency * time);
+    }
+}
+
+function onFrequencyChanged() {
+    const slider = document.getElementById("frequencySlider");
+    frequency = slider.value;
+    const label = document.getElementById("frequencyLabel");
+    label.innerText = frequency + " Hz";
+}
+
+function onAmplitudeChanged() {
+    const slider = document.getElementById("amplitudeSlider");
+    amplitude = slider.value / 100.0;
+    const label = document.getElementById("amplitudeLabel");
+    label.innerText = "" + amplitude;
 }
