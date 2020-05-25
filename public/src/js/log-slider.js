6222740d7368e05cf5c21b6532ec5b710f6fee80
Jack-42 <jakob-robert@hotmail.de>
2020-05-25 20:34:18 +0200

    Simplify log slider calculations

--- public/src/js/log-slider.js
+++ public/src/js/log-slider.js
@@ -1,18 +1,16 @@
 class LogSlider {
     constructor(maxPos, minVal, maxVal) {
         this.maxPos = maxPos;
-        this.minLogVal = Math.log(minVal);
-        this.maxLogVal = Math.log(maxVal);
-        this.scale = (this.maxLogVal - this.minLogVal) / this.maxPos;
+        this.minVal = minVal;
+        this.maxVal = maxVal;
+        this.scale = Math.log(this.maxVal / this.minVal) / this.maxPos;
     }
 
     getValueFromPosition(position) {
-        // TODO: refactor -> multiply result by minVal?
-        return Math.exp(position * this.scale + this.minLogVal);
+        return Math.exp(position * this.scale) * this.minVal;
     }
 
     getPositionFromValue(value) {
-        // TODO: refactor -> divide value by minVal?
-        return (Math.log(value) - this.minLogVal) / this.scale;
+        return Math.log(value / this.minVal) / this.scale;
     }
 }
